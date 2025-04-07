56ecb3754f6be2ea4999555eeb407a8f85995c3d
Ignacio Heredia <iheredia@ifca.unican.es>
2025-04-07 11:16:34 +0200

    fix: update remove tool script

--- utils/remove-submodule.sh
+++ utils/remove-submodule.sh
@@ -3,7 +3,7 @@
 set -xe
 
 git config -f .gitmodules --remove-section submodule.$1
-git config -f .git/config --remove-section submodule.$1
+# git config -f .git/config --remove-section submodule.$1
 git add .gitmodules
 git rm --cached $1
 git commit -m "Remove submodule in $1"
